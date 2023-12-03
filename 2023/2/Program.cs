5a53e5e567d388548908a28850b8d1ff4a8d0540
hecd <hecd744@gmail.com>
2023-12-03 12:56:21 +0100

    Refactors day 2 into a part separated method solution.

--- 2023/2/Program.cs
+++ 2023/2/Program.cs
@@ -1,28 +1,37 @@
 ﻿using System.Text.RegularExpressions;
 
+static int GetImpossibleGamesGameIdSum(Dictionary<string, int> maxValues, IEnumerable<string> lines)
+{
+    return lines.Select((line, lineNumber) =>
+    {
+        var isImpossible = new[] { "red", "green", "blue" }.Any(color =>
+            new Regex($@"(\d+) {color}")
+                .Matches(line)
+                .Select(m => int.Parse(m.Groups[1].Value))
+                .Any(count => count > maxValues[color])
+        );
+        return isImpossible ? 0 : lineNumber + 1;
+    }).Sum();
+}
+
+static int GetMinimumCubeColorPowerSum(IEnumerable<string> lines)
+{
+    return lines.Select(line =>
+    {
+        var minByColor = new[] { "red", "green", "blue" }.Select(color =>
+            new Regex($@"(\d+) {color}")
+                .Matches(line)
+                .Select(m => int.Parse(m.Groups[1].Value))
+                .Max(m => m)
+        );
+        return minByColor.Aggregate((acc, x) => acc * x);
+    }).Sum();
+}
+
 var maxValues = new Dictionary<string, int> { ["red"] = 12, ["green"] = 13, ["blue"] = 14 };
 var lines = Utils.Utils.GetLines("input.txt");
-var possibleGameIdsSum = lines.Select((line, lineNumber) =>
-{
-    var isImpossible = new[] { "red", "green", "blue" }.Any(color =>
-        new Regex($@"(\d+) {color}")
-            .Matches(line)
-            .Select(m => int.Parse(m.Groups[1].Value))
-            .Any(count => count > maxValues[color])
-    );
-    return isImpossible ? 0 : lineNumber + 1;
-}).Sum();
+int partOne = GetImpossibleGamesGameIdSum(maxValues, lines);
+int partTwo = GetMinimumCubeColorPowerSum(lines);
 
-var productOfSets = lines.Select((line) =>
-{
-    var minByColor = new[] { "red", "green", "blue" }.Select(color =>
-        new Regex($@"(\d+) {color}")
-            .Matches(line)
-            .Select(m => int.Parse(m.Groups[1].Value))
-            .Max(m => m)
-    );
-    return minByColor.Aggregate((acc, x) => acc * x);
-}).Sum();
-
-Console.WriteLine(possibleGameIdsSum);
-Console.WriteLine(productOfSets);
+Console.WriteLine(partOne);
+Console.WriteLine(partTwo);
